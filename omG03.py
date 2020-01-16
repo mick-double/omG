87b0477714024c9b87538462393fd15c8ef36016
mick-double <mitsuya08@gmail.com>
2020-01-16 12:35:28 +0900

    入力文字の位置が気に入らなかったので 修正
    空白を３つ先に入れて改行なしに
    インプットさせた
    
    こういうのが工夫なんだろうなぁ
    プログラムの
    
    作りに合わせなきゃいけないところ
    inputにもフォーマットがあればいいのに

--- omG03.py
+++ omG03.py
@@ -1,19 +1,23 @@
 # 最初に考えた数字当てクイズ
 import random
 print('5回のうちに1から6までの数字を当ててください')
+print('   ',end='')
 answer = random.randint(1,7)
 inputnum = []
 for i in range(5) :
     inputnum.append(int(input()))
     if inputnum[i] == answer :
         print('やった！正解です')
-        print()
+        print('   ',end='')
         break
     elif i < 4:
         print('もう一回')
+        print('   ',end='')
     else:
         print('残念でした')
+        print('   ',end='')
 print()
 print('正解は',answer)
 print('打ち込み履歴は')
+print('   ',end='')
 print(inputnum)
