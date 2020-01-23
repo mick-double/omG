4f41887ec7dfc07e03b0cc67324a3daeea5899a1
mick-double <mitsuya08@gmail.com>
2020-01-24 00:30:37 +0900

    closeをやめてwithにした

--- inputf.py
+++ inputf.py
@@ -1,24 +1,20 @@
 # fileを読み込んで文字列検索する
 # とりあえずtakaraがいくつあるかを読み込む
+# with を使ってcloseをなくした withを使うと例外が起きてもcloseしてくれる
 
-f = open('txtfile.txt','r')
+with open('txtfile.txt','r') as f :
+    l = f.readlines()
 
-# ファイルの内容表示
-# print(f.read())
+    # fileの長さを表示
+    print('行数は',len(l))
 
-# リストに読み込む
+    count = 0
+    for n in range(len(l)):
+        print(l[n].strip())
+        if  'takara' in l[n]:
+            count = count + 1
 
-l = f.readlines()
+    print('takaraの数は',count)
 
-# fileの長さを表示
-print('行数は',len(l))
 
-count = 0
-for n in range(len(l)):
-    print(l[n].strip())
-    if  'takara' in l[n]:
-        count = count + 1
 
-print('takaraの数は',count)
-
-f.close()
