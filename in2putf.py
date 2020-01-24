8337d2c8e8192fa018280eb003c0a770da326d23
mick-double <mitsuya08@gmail.com>
2020-01-25 00:13:36 +0900

    全部ファイルを読み込んで、いっぺんにカウントする方法でやってみた。あと、文字の置換も。

---
+++ in2putf.py
@@ -0,0 +1,21 @@
+# fileを読み込んで文字列検索する
+# とりあえずtakaraがいくつあるかを読み込む
+# with を使ってcloseをなくした withを使うと例外が起きてもcloseしてくれる
+# もうちょっとスマートな方法でやってみる　countを使う
+# takaraはomankoに置換（ちかん）
+
+with open('txtfile.txt','r') as f :
+    with open('otextfile.txt','w') as of:
+        count = 0
+        ocount = 0
+        l = f.read()
+        count = l.count('takara')
+        of = l.replace('takara','omanko')
+        ocount = of.count('omanko')
+        print(of)
+
+
+print('txtfile.txtの中のtakaraの数は',count)
+print('otextfile.txtの中のomankoの数は',ocount)
+
+
